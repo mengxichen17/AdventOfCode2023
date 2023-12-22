efa0207308fcdf7b7dc9c645b751f0a325243ba7
mengxichen17 <yjjslsyq@gmail.com>
2023-12-22 15:36:03 -0800

    started  day 3 part 2

--- Day3/solution2.py
+++ Day3/solution2.py
@@ -1,32 +1,37 @@
-# from string import punctuation
+import collections
 def solution():
 	def isPartNumber(i, j):
 		surrounding = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+		list_of_symbols = set()
 		for x, y in surrounding:
 			if 0 <= x + i < rows and 0 <= y + j < cols and not input[x+i][y+j].isalnum() and input[x+i][y+j] != '.':
-				return True
-		return False
+				list_of_symbols.add((x+i, y+i))
+		return list_of_symbols
 
 	input = []
-	with open('input.txt') as f:
+	with open('sample.txt') as f:
 		input = [list(line.strip()) for line in f]
 	rows, cols = len(input), len(input[0])
+	gears = collections.defaultdict(list) # key - location of special symbol; value - list of numbers using this special symbol
 	answer = 0
 	curr_num = ''
-	part_num = False 
+	adj_symbols = set()
 	for i in range(rows):
 		for j in range(cols):
 			char = input[i][j]
 			if char.isdigit():
 				curr_num += char
-				if not part_num:
-					part_num = isPartNumber(i, j)
+				adj_symbols.update(isPartNumber(i, j))
 			if j == cols - 1 or not char.isdigit():
-				if curr_num and part_num:
-					# print(curr_num)
-					answer += int(curr_num)
+				if curr_num and adj_symbols:
+					# print(curr_num, adj_symbols)
+					for a, b in adj_symbols:
+						gears[(a, b)].append(int(curr_num))
 				curr_num = ''
-				part_num = False
+				adj_symbols = set()
+	for key, lst_num in gears.items():
+		if len(lst_num) == 2:
+			answer += lst_num[0] * lst_num[1]
 
 	print(answer)
 
