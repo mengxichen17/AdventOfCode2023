60afe80b0b1397ec7af5409556f7ae7a3621d889
mengxichen17 <yjjslsyq@gmail.com>
2023-12-22 15:45:18 -0800

    finished  day 3 part 2

--- Day3/solution2.py
+++ Day3/solution2.py
@@ -5,11 +5,11 @@ def solution():
 		list_of_symbols = set()
 		for x, y in surrounding:
 			if 0 <= x + i < rows and 0 <= y + j < cols and not input[x+i][y+j].isalnum() and input[x+i][y+j] != '.':
-				list_of_symbols.add((x+i, y+i))
+				list_of_symbols.add((x+i, y+j))
 		return list_of_symbols
 
 	input = []
-	with open('sample.txt') as f:
+	with open('input.txt') as f:
 		input = [list(line.strip()) for line in f]
 	rows, cols = len(input), len(input[0])
 	gears = collections.defaultdict(list) # key - location of special symbol; value - list of numbers using this special symbol
@@ -29,6 +29,7 @@ def solution():
 						gears[(a, b)].append(int(curr_num))
 				curr_num = ''
 				adj_symbols = set()
+	# print(gears)
 	for key, lst_num in gears.items():
 		if len(lst_num) == 2:
 			answer += lst_num[0] * lst_num[1]
